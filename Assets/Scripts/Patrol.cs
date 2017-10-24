ee6dab28e5082317e0e83dcd580c736ab63af3ae
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-10-23 23:51:50 -0700

    update

---
+++ Assets/Scripts/Patrol.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour {
+	public Transform[] patrolPoints;
+	private int currPoint;
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		transform.position = patrolPoints[0].position;
+		currPoint = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position == patrolPoints[currPoint].position)
+		{
+			currPoint++;
+			if(currPoint >= patrolPoints.Length)
+				currPoint = 0;
+		}
+		transform.position = Vector3.MoveTowards(transform.position, patrolPoints[currPoint].position, 
+													moveSpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	public float moveSpeed;
+	private CharacterController characterController;
+
+	// Use this for initialization
+	void Start () {
+		characterController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		characterController.SimpleMove(moveDirection * moveSpeed);
+	}
+}
