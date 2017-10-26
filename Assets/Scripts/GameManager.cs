51cd662e384d80d9c87e1c9a24410d1006fa4079
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-10-25 23:19:13 -0700

    can work

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+	public static int currentScore;
+	public static int highScore;
+
+	public static int currentLevel = 0;
+	public static int unlockedLevel;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public static void CompleteLevel(){
+		if( currentLevel < 2)
+		{
+			currentLevel++;
+			SceneManager.LoadScene(currentLevel);
+		}
+		else
+		{
+			print ("You win");
+		}
+	}
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,36 +4,50 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 	public float moveSpeed;
-	private CharacterController characterController;
+	private float maxSpeed = 10f;
 	private Vector3 spawn;
 
 	public GameObject deathParticles;
 	
 	// Use this for initialization
 	void Start () {
-		characterController = GetComponent<CharacterController>();
 		spawn = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-		characterController.SimpleMove(moveDirection * moveSpeed);
+		if(GetComponent<Rigidbody>().velocity.magnitude < maxSpeed)
+		{
+			Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			// GetComponent<Rigidbody>().velocity = movement * moveSpeed; 
+			GetComponent<Rigidbody>().AddRelativeForce(movement * moveSpeed);  // based on player's rotation
+		}
+
+		if(transform.position.y < -2)
+		{
+			Die();
+		}
 	}
 
 	void OnCollisionEnter(Collision other)
 	{
 		if(other.transform.tag == "Enemy")
 		{
-			print("Colision");
-			Instantiate(deathParticles, transform.position, Quaternion.Euler(270, 0, 0));
-			transform.position = spawn;
+			Die();
 		}
 	}
 
 	void Die(){
 		Instantiate(deathParticles, transform.position, Quaternion.Euler(270, 0, 0));
 		transform.position = spawn;
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.transform.tag == "Goal")
+		{
+			GameManager.CompleteLevel();
+		}
 	}
 }
